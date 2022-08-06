1e0ff8222580c9722cf1d6829801a1262590f6ea
MiAnClGr <MiAnClGr@gmail.com>
2022-08-06 13:56:34 +1000

    Changed Background and brought in a seperate logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Main from './components/Main.js'
+import Logo from './components/Logo.js'
 
 
 function App() {
   return (
     <div className="App">
+      <Logo/>
       <Main/>
       
      
---
+++ src/components/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../zong-logo.png'
+
+const Logo = () => {
+  return (
+   
+    <img className= 'Zong-logo' src= {logo}/>
+    
+  )
+}
+
+export default Logo
