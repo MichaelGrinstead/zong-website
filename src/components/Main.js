0625b0c0f4edffc9cab7bdb3d761eedaf0495f06
MiAnClGr <MiAnClGr@gmail.com>
2022-08-06 13:18:37 +1000

    Update css for mobile

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,27 @@
 import React from 'react'
 import serpentSingle from '../serpent-single.jpg'
-
+import { IoIosMenu } from 'react-icons/io'
+ 
 
 const Main = () => {
   return (
     <div className= 'Main'>
 
-        <div className= 'Menu'>
-            <h3>Home</h3>
-            <h3>Tour</h3>
-            <h3>Merch</h3>
-            <h3>Media</h3>
-            <h3>About</h3> 
-        </div>
+      <IoIosMenu className= 'Mobile-Menu'/>
 
-        <h1 className= 'Serpent-single-title'>New Single "The Serpent" Out Now</h1>
+      <div className= 'Menu'>
+          <h3>Home</h3>
+          <h3>Tour</h3>
+          <h3>Merch</h3>
+          <h3>Media</h3>
+          <h3>About</h3> 
+          
+      </div>
 
-        <img className='Serpent-single' src={serpentSingle} />
+
+      <h1 className= 'Serpent-single-title'>New Single "The Serpent" Out Now</h1>
+
+      <img className='Serpent-single' src={serpentSingle} />
     </div>
   )
 }
