52c62134c0471affc396d6a0fe378b86943ec61d
MiAnClGr <MiAnClGr@gmail.com>
2022-08-06 10:52:38 +1000

    First commit

---
+++ src/components/Main.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import serpentSingle from '../serpent-single.jpg'
+
+
+const Main = () => {
+  return (
+    <div className= 'Main'>
+
+        <div className= 'Menu'>
+            <h3>Home</h3>
+            <h3>Tour</h3>
+            <h3>Merch</h3>
+            <h3>Media</h3>
+            <h3>About</h3> 
+        </div>
+
+        <h1 className= 'Serpent-single-title'>New Single "The Serpent" Out Now</h1>
+
+        <img className='Serpent-single' src={serpentSingle} />
+    </div>
+  )
+}
+
+export default Main
